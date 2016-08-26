fc4e0cc17b4041042479e78a1ef567f183ba4e55
Oladeji Femi <femidotexe@gmail.com>
2016-08-26 08:21:44 +0100

    Switched to ejs rendering mechanism and finished the design of the dashboard

--- routes/routing.js
+++ routes/routing.js
@@ -1,9 +1,12 @@
 var jwt = require('jsonwebtoken');
 var express = require('express');
 var UserController = require('./controllers/usercontroller');
+var StoreController = require('./controllers/storecontroller');
+
 
 // instantiate the usercontroller
 var user = new UserController();
+var store = new StoreController();
 
 var routes = function(app) {
   app.post('/api/login', user.login);
@@ -14,13 +17,13 @@ var routes = function(app) {
   // route middleware to verify token
   apiRoutes.use(function(req, res, next) {
     var token = req.body.token || req.query.q || req.headers['x-access-token'];
-
     if(token) {
       jwt.verify(token, 'secret', function(err, decoded) {
         if(err) {
           return res.json({status:false, message:'Token authentication failed'});
         } else {
           req.decoded = decoded;
+          req.token = token;
           next();
         }
       })
@@ -35,6 +38,12 @@ var routes = function(app) {
   /*app.get('/', function(req, res) {
     console.log(req.decoded);
   });*/
+
+  app.get('/api/store', store.getStores);
+
+  app.post('/api/store', store.createStore)
+
+  app.get('/api/user', user.getUserInfo);
 }
 
 module.exports = routes;
