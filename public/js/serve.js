f1b8653631aeb9562022d134cb779a6c0375e635
Oladeji Femi <femidotexe@gmail.com>
2016-08-28 04:09:11 +0100

    Product editing can now be done

--- public/js/serve.js
+++ public/js/serve.js
@@ -43,32 +43,52 @@ $(document).ready(function() {
           })
         }
       }
-    } else if(domid == 'createproduct') {
+    } else if(domid == 'createproduct' || domid == 'editproduct') {
       var formFieldIds = ['product_name', 'product_desc', 'price', 'stores_id', 'category_id'];
       var data = myGlobal.getFormData(formFieldIds);
-      if(data === 'null') responseDom.text('All fields are compulsory').addClass('label-danger');
+      if(data === 'null' || $('#productimage')[0].files[0] == undefined) responseDom.text('All fields are compulsory').addClass('label-danger');
       else {
         var formdata = new FormData()
         formdata.append('product_image', $('#productimage')[0].files[0]);
         $.each(data, function(key, value) {
           formdata.append(key, value);
         });
-        var apiresponse = myGlobal.advanceAjax(formdata, '/api/product', $(this));
-        apiresponse.done(function(reply) {
-          if(reply == undefined) {
-            responseDom.text('An error occurred, please try again').removeClass('label-success').addClass('label-danger');
-          } else {
-            if(reply.status == false) {
-              responseDom.text('Invalid product image').removeClass('label-success').addClass('label-danger');
+        if(domid == 'createproduct') {
+          var apiresponse = myGlobal.advanceAjax(formdata, '/api/product', $(this));
+          apiresponse.done(function(reply) {
+            if(reply == undefined) {
+              responseDom.text('An error occurred, please try again').removeClass('label-success').addClass('label-danger');
             } else {
-              responseDom.text('Product Successfully created').removeClass('label-danger').addClass('label-success');
-              $('#productmodal').modal('hide');
-              setTimeout(function() {
-                myGlobal.renderPage('/products');
-              }, 500);
+              if(reply.status == false) {
+                responseDom.text('Invalid product image').removeClass('label-success').addClass('label-danger');
+              } else {
+                responseDom.text('Product Successfully created').removeClass('label-danger').addClass('label-success');
+                $('#productmodal').modal('hide');
+                setTimeout(function() {
+                  myGlobal.renderPage('/products');
+                }, 500);
+              }
             }
-          }
-        })
+          })
+        } else {
+          var apiresponse = myGlobal.advanceAjax(formdata, '/api/updateproduct/'+$(this).attr('productid'), $(this));
+          apiresponse.done(function(reply) {
+            if(reply == undefined) {
+              responseDom.text('An error occurred, please try again').removeClass('label-success').addClass('label-danger');
+            } else {
+              if(reply.status == false) {
+                responseDom.text('Invalid product image').removeClass('label-success').addClass('label-danger');
+              } else {
+                responseDom.text('Product Successfully edited').removeClass('label-danger').addClass('label-success');
+                $('#productmodal').modal('hide');
+                setTimeout(function() {
+                  myGlobal.renderPage('/products');
+                }, 500);
+              }
+            }
+          })
+        }
+        
       }
     } else if(domid == 'updateuser') {
       var responseDom = $(this).siblings('.message');
@@ -105,5 +125,55 @@ $(document).ready(function() {
       $('.edit').show();
       index++;
     }
+  });
+
+  $('#page-inner').delegate('.editproduct', 'click', function(e) {
+    $('.create').hide();
+    $('.edit').attr('productid', $(this).attr('id'));
+    $('.edit').show();
+    var tdAll = $(this).parents('tr').find('td');
+    var fields = {product_name:'', stores_id: '', product_desc:'', price:'', category_id:''}
+    var index = 0;
+    for(var key in fields) {
+      if(key === 'product_desc') {
+        $('#'+key).text(tdAll.eq(index).text());
+      } else if(key === 'stores_id') {
+        var storename = tdAll.eq(index).text();
+        $('#stores_id').find('option').each(function() {
+          if($(this).text() === storename) {
+            $(this).attr('selected', 'selected');
+          }
+        })
+      } else if(key === 'category_id') {
+        var category = tdAll.eq(index).text();
+        $('#category_id').find('option').each(function() {
+          if($(this).text() === category) {
+            $(this).attr('selected', 'selected');
+          }
+        })
+      } else {
+        $('#'+key).attr('value', tdAll.eq(index).text());
+      }
+      index++;
+    }
+
+  });
+
+
+ $('#page-inner').delegate('#createstoremodal', 'click', function(e) {
+    $('#storemodal').find('input').attr('value', '');
+    $('#storemodal').find('textarea').text('');
+    $('.create').show();
+    $('.edit').hide();
+  });
+
+ $('#page-inner').delegate('#createproductmodal', 'click', function(e) {
+    $('#productmodal').find('input').attr('value', '');
+    $('#productmodal').find('textarea').text('');
+    $('productmodal').find('option').removeAttr('selected');
+    $('#productmodal').find('#stores_id').children('option').eq(0).attr('selected', 'selected');
+    $('#productmodal').find('#category_id').children('option').eq(0).attr('selected', 'selected');
+    $('.create').show();
+    $('.edit').hide();
   });
 })
